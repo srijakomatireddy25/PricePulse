d2e8f42d95787ae3037fa552a55a2cdd660628d6
srijakomatireddy25 <srijakomatireddy25@gmail.com>
2026-04-06 16:57:45 +0530

    fixed

--- pricepulse/src/main/java/com/example/demo/config/CorsConfig.java
+++ pricepulse/src/main/java/com/example/demo/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","http://localhost:5174")
+                        .allowedOrigins("http://localhost:5173","http://localhost:5174","https://price-pulse-zeta.vercel.app")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
